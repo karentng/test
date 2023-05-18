97b4836424edf0e1a515f43832edb5c7d7358843
Karent Narvaez <karentnathalia@gmail.com>
2023-05-18 13:03:13 -0400

    Mocking a function to run tests

--- script2.test.js
+++ script2.test.js
@@ -3,7 +3,7 @@ const swapi = require('./script2');
 
 describe('swapi', () => {
   
-  it('getPeople pulls properly data', ()=> {
+  it('getPeople pulls properly data', () => {
     expect.assertions(2);
     return swapi.getPeople(fetch).then(data => {
       expect(data.count).toEqual(87);
@@ -11,12 +11,30 @@ describe('swapi', () => {
     })
   });
 
-  it('getPeoplePromise pulls properly data', ()=> {
+  it('getPeoplePromise pulls properly data', () => {
     expect.assertions(2);
     return swapi.getPeoplePromise(fetch).then(data => {
       expect(data.count).toEqual(87);
       expect(data.results.length).toBeGreaterThan(5);
     })
   });
+
+  const mockFetch = jest.fn()
+    .mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+        count: 89,
+        results: [1, 2, 3, 4, 5, 6]
+      })
+    }))
+
+  it('getPeople returns proper data with mock', () => {
+    expect.assertions(4);
+    return swapi.getPeople(mockFetch).then(data => {
+      expect(data.count).toEqual(89);
+      expect(data.results.length).toBeGreaterThan(5);
+      expect(mockFetch.mock.calls.length).toBe(1);
+      expect(mockFetch).toBeCalledWith('http://swapi.py4e.com/api/people');
+    })
+  })
 
 });
